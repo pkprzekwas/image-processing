20c1c79d70c4e32b4d07d91e44db107fc390f4e8
Patryk Przekwas <pkprzekwas@gmail.com>
2018-01-23 23:12:42 +0100

    Translation

---
+++ front/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='footer bg-inverse'>
+      <div className='text-right'>
+        <span className='text-muted'>Patryk Przekwas | Maciej Wysocki</span><br/>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
--- front/src/index.js
+++ front/src/index.js
@@ -4,11 +4,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import HomePage from './components/HomePage';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 ReactDOM.render(
   <div>
     <Header/>
     <HomePage />
+    <Footer/>
   </div>,
   document.getElementById('root')
 );
